961ad0174f65346e945127097ed50e7e5ec89a0f
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-05 15:51:51 +0100

    Header is done

--- FitnessApp/src/app/header/header.ts
+++ FitnessApp/src/app/header/header.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
 }
