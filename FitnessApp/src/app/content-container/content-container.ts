36cb50cfbc111c91606a45f7ef4bb60dd4976fe8
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-05 15:17:16 +0100

    Createdall 3 compnents for this project and added fontawesome

---
+++ FitnessApp/src/app/content-container/content-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-container',
+  imports: [],
+  templateUrl: './content-container.html',
+  styleUrl: './content-container.css',
+})
+export class ContentContainer {
+
+}
